498039c0b7f57a497c7b59c11cbca2039ccdb92a
ShreeSharma31 <shree.31sharma@gmail.com>
2025-08-04 19:04:30 +0530

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -61,17 +61,6 @@ export default function TeamPage() {
         },
       ],
     },
-     {
-      title: "Student Advisor",
-      members: [
-        {
-          name: "Harshita",
-          position: "#",
-          image: "/harshita.png",
-          email: "harshita@ecell.cgc.edu.in",
-        },
-      ],
-    },
     {
       title: "Documentation and Operations Team",
       members: [
